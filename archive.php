4f9e1c45cf54805b8bfa9f8303fd82816ae36b21
mac_a2141 <victor@piousbox.com>
2022-09-27 17:42:12 -0500

    better and better

--- archive.php
+++ archive.php
@@ -1,37 +1,38 @@
 <?php
 /**
  * The template for displaying archive pages
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package WordPress
- * @subpackage Twenty_Twenty_One
- * @since Twenty Twenty-One 1.0
  */
 
 get_header();
-
+$page_object = get_queried_object();
 $description = get_the_archive_description();
 ?>
 
 <?php if ( have_posts() ) : ?>
 
-	<header class="page-header alignwide">
-		<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
-		<?php if ( $description ) : ?>
-			<div class="archive-description"><?php echo wp_kses_post( wpautop( $description ) ); ?></div>
-		<?php endif; ?>
-	</header><!-- .page-header -->
+  <header class="page-header ">
+    <div class='page-header-inner '>
+      <h1 class="entry-title alignwide"><?= $page_object->cat_name; ?></h1>
 
-	<?php while ( have_posts() ) : ?>
-		<?php the_post(); ?>
-		<?php get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) ); ?>
-	<?php endwhile; ?>
+      <?php if ( $description ) : ?>
+        <div class="archive-description"><?php echo wp_kses_post( wpautop( $description ) ); ?></div>
+      <?php endif; ?>
 
-	<?php twenty_twenty_one_the_posts_navigation(); ?>
+    </div>
+  </header>
+
+  <div class='alignwide articles' >
+    <?php while ( have_posts() ) : ?>
+      <?php the_post(); ?>
+      <?php get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) ); ?>
+    <?php endwhile; ?>
+    <?php twenty_twenty_one_the_posts_navigation(); ?>
+  </div>
 
 <?php else : ?>
-	<?php get_template_part( 'template-parts/content/content-none' ); ?>
+  <div class='alignwide' >
+    <?php get_template_part( 'template-parts/content/content-none' ); ?>
+  </div>
 <?php endif; ?>
 
 <?php get_footer(); ?>
--- template-parts/content/content-page.php
+++ template-parts/content/content-page.php
@@ -6,21 +6,21 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-  <?php if ( ! is_front_page() ) : ?>
-    <header class="entry-header"
+  <?php if ( !is_front_page() ) : ?>
+    <header class="page-header"
       style="background-image: url('<?= get_post_meta( get_the_ID(), 'wco_hero_url', true) ?>'); "
     >
-      <div class="entry-header-inner">
+      <div class="page-header-inner">
         <h1 class="entry-title alignwide"><? the_title(); ?></h1>
       </div>
     </header>
   <?php elseif ( has_post_thumbnail() ) : ?>
-    <header class="entry-header alignwide">
+    <header class="page-header alignwide">
       <?php twenty_twenty_one_post_thumbnail(); ?>
     </header>
   <?php endif; ?>
 
-  <div class="entry-content">
+  <div class="entry-content <?= is_front_page() ? '' : 'alignwide overflow-margin' ?>" ><!-- this cannot be alignwide b/c it used by homepage -->
     <?php
     the_content();
 
