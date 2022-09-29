eac3304527c4d340008928840ce2f8068743f7ee
mac_a2141 <victor@piousbox.com>
2022-09-29 16:15:45 -0500

    the default  admin-bar has very poor architecture and I had to change it

--- functions.php
+++ functions.php
@@ -179,6 +179,12 @@ function wco_admin_styles() {
 }
 add_action('admin_enqueue_scripts', 'wco_admin_styles');
 
+function my_filter_head() {
+  remove_action('wp_head', '_admin_bar_bump_cb');
+}
+add_action('get_header', 'my_filter_head');
+
+
 /**
  * Register widget area.
  */
