19f44f6eabfa982df9fb1223353eae367be863c9
mac_a2141 <victor@piousbox.com>
2022-10-04 21:58:25 -0500

    google analytics is back in

--- header.php
+++ header.php
@@ -10,18 +10,28 @@
 <!doctype html>
 <html <?php language_attributes(); ?> >
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<?php wp_head(); ?>
+  <meta charset="<?php bloginfo( 'charset' ); ?>" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <?php wp_head(); ?>
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-53077236-2"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-53077236-2');
+  </script>
+
 </head>
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content">Skip to content</a>
+  <a class="skip-link screen-reader-text" href="#content">Skip to content</a>
 
-	<?php get_template_part( 'template-parts/header/site-header' ); ?>
+  <?php get_template_part( 'template-parts/header/site-header' ); ?>
 
-	<div id="content" class="site-content">
-		<div class="content-area">
-			<main class="site-main">
+  <div id="content" class="site-content">
+    <div class="content-area">
+      <main class="site-main">
