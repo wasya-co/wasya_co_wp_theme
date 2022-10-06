6ecd69efd75010296cd801c6d88769783d91a06b
mac_a2141 <victor@piousbox.com>
2022-10-06 13:34:05 -0500

    tracking on the resume page

--- page-resume.php
+++ page-resume.php
@@ -1,16 +1,39 @@
-<style>
-html {
-  overflow: hidden;
-}
-</style>
-
 <?php
 /**
  * Template Name: resume-page
  *
  * _vp_ 2022-10-06
  */
+?>
 
-the_post();
-get_template_part( 'template-parts/content/content-resume' );
+<html>
+<head>
+  <title>Victor Pudeyev - Software Engineering Lead</title>
+
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-53077236-2"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-53077236-2');
+  </script>
+
+<style>
+html {
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+}
+
+</style>
+</head>
+<body>
+<?
+  the_post();
+  get_template_part( 'template-parts/content/content-resume' );
+?>
+</body>
+</html>
+
 
