5b5c4079fe339873ff1f3d6a8f11a4895404fe3c
mac_a2141 <victor@piousbox.com>
2022-10-15 14:29:31 -0500

    owa code

--- header.php
+++ header.php
@@ -14,6 +14,28 @@
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <?php wp_head(); ?>
 
+
+<!-- OWA Tracker -->
+<script type="text/javascript">
+//<![CDATA[
+var owa_baseUrl = 'https://owa.wasya.co/';
+var owa_cmds = owa_cmds || [];
+owa_cmds.push(['setSiteId', '00cc61b2003af4e35fa914b1733c02ba']); // wasya_co
+owa_cmds.push(['trackPageView']);
+owa_cmds.push(['trackClicks']);
+
+(function() {
+    var _owa = document.createElement('script'); _owa.type = 'text/javascript'; _owa.async = true;
+    owa_baseUrl = ('https:' == document.location.protocol ? window.owa_baseSecUrl || owa_baseUrl.replace(/http:/, 'https:') : owa_baseUrl );
+    _owa.src = owa_baseUrl + 'modules/base/dist/owa.tracker.js';
+    var _owa_s = document.getElementsByTagName('script')[0]; _owa_s.parentNode.insertBefore(_owa, _owa_s);
+}());
+//]]>
+</script>
+<!-- End OWA Tracker -->
+
+
+
   <!-- Global site tag (gtag.js) - Google Analytics -->
   <script async src="https://www.googletagmanager.com/gtag/js?id=UA-53077236-2"></script>
   <script>
