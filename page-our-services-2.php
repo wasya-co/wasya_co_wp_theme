fb9640f0422167b49f9d061d1428fea6ddee2b29
mac_a2141 <victor@piousbox.com>
2023-02-12 22:32:05 -0500

    groking page our-services-2

---
+++ page-our-services-2.php
@@ -0,0 +1,292 @@
+<!doctype html>
+<html <? language_attributes(); ?>>
+
+<head>
+  <link rel="icon" type="image/x-icon" href="./favicon.png">
+  <title>Our Services - Software Development consulting @ WasyaCo</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <meta name="description" content="Agile scrum methodology. Rapid iterations on software development! Ruby on rails, react, aws, cloud-based free services." />
+  <meta name="keywords" content="software development, wasya co, victor piousbox" />
+  <meta name="author" content="Victor Piousbox" />
+
+  <meta name="copyright" content="WasyaCo" />
+  <meta name="robots" content="index,follow" />
+
+  <!--Bootstrap-->
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+
+  <link rel="stylesheet" href="/wp-content/themes/wasya_co_wp_theme/assets/css-compiled/a20230212-our-services-2.css" crossorigin="anonymous">
+
+  <? wp_head(); ?>
+  <style>
+    a {
+      text-decoration: underline;
+    }
+  </style>
+</head>
+
+<body <? body_class(); ?>>
+
+<? get_template_part( 'template-parts/header/site-header' ); ?>
+
+  <div id='a20230223-our-services-2'>
+
+    <!-- UIUX Graphic Design
+Frontends
+Backends & Application layer
+Data Science, databases, ML
+Infrastructure, cloud, AWS
+DNS SMTP
+
+separately: marketing, strategy, analysis, business development -->
+
+    <!-- <section>
+  Intellectual Property (IP) & Content Creation
+
+  Illustration
+    logos
+
+  Copy Writing
+
+  3d assets - threejs, low-poly, rigged, web 3d
+</section> -->
+
+    <div class='spacer-300'></div>
+
+    <section class='dev-app-layer dev-app-layer-sm'>
+      <div class='header max-width'>
+        <h2 class='title'>
+          Application Layer Development
+        </h2>
+        <div class='fancy-border'></div>
+      </div>
+
+      <div class='row max-width'>
+        <div class='col-md-4'>
+
+          <div class='wco-card-3 dev-ror'>
+
+            <div class='illustration'>
+            </div>
+
+            <h2>Ruby on Rails</h2>
+
+            <a href="/our-process-2"><button class="button-91" role="button">Explore</button></a>
+
+            <!-- <div class='related-services'>
+              <h4>Related services:</h4>
+              <ol>
+                <li><a href='#'>UIUX</a></li>
+                <li><a href='#'>Cloud Infrastructure & Devops</a></li>
+                <li><a href='#'>Maintenance of Legacy Code</a></li>
+              </ol>
+            </div> -->
+
+            <!-- <div class='from-our-blog'>
+              <h4>From our engineering blog:</h4>
+              <ol>
+                <li><a href='#'>Ruby templating: erb vs haml vs slim: an opinion</a></li>
+                <li><a href='#'>How to split an app into gems</a></li>
+                <li><a href='#'>Increasing prototyping speed: sql vs nosql</a></li>
+              </ol>
+            </div> -->
+
+          </div><!--wco-card-3-->
+
+        </div>
+        <div class='col-md-4'>
+
+          <div class='wco-card-3 dev-wp'>
+
+            <div class='illustration'></div>
+
+            <h2>Wordpress</h2>
+
+            <a href="/our-process-2"><button class="button-91" role="button">Bugfixes & Updates</button></a>
+
+            <!-- Related services:
+            php
+            cakePHP
+            php testing w/composer -->
+
+            <!-- <div class='from-our-blog'>
+              <h4>From our engineering blog:</h4>
+              <ol>
+                <li><a href='#'>Setting up testing infrastructure for wordpress plugin development</a></li>
+                <li><a href='#'>Deploying wordpress in the cloud with Docker</a></li>
+                <li><a href='#'>Show More</a></li>
+              </ol>
+            </div> -->
+
+          </div><!--wco-card-3-->
+
+        </div>
+        <div class='col-md-4'>
+
+          <div class='wco-card-3 dev-react'>
+
+            <div class='illustration'></div>
+
+            <h2>React Js</h2>
+
+            <a href="/our-process-2"><button class="button-91" role="button">Build a Frontend</button></a>
+
+            <!-- Related Services:
+          node
+          next_js
+          es6 & babel
+          socket & web rtc -->
+
+            <!-- <div class='from-our-blog'>
+              <h4>From our engineering blog:</h4>
+              <ol>
+                <li><a href='#'>Configure webpack build pipeline in production</a></li>
+                <li><a href='#'>Resilience & future-proofing: typescript vs javascript</a></li>
+                <li><a href='#'>Show More</a></li>
+                <li><a href='#'>Programming web sockets for RTC</a></li>
+                <li><a href='#'>The NODE_ENV problem with CRA</a></li>
+              </ol>
+            </div> -->
+
+          </div><!--wco-card-3-->
+        </div>
+      </div><!--row-->
+    </section><!--dev-app-layer-sm-->
+
+    <div class='spacer-500'></div>
+
+    <section class='dev-app-layer dev-app-layer-md'>
+      <div class='header max-width'>
+        <h2 class='title'>
+          Application Layer Development
+        </h2>
+        <div class='fancy-border'></div>
+      </div>
+
+      <div class='max-width'>
+
+          <div class='wco-card-4 dev-ror'>
+
+            <div class='illustration'></div>
+
+            <div class='col-1'>
+
+              <div class='header'>
+                <h2>Ruby on Rails</h2>
+                <a href="/our-process-2"><button class="button-91" role="button">Explore</button></a>
+                <!-- <a href='/'>View Offered Services</a> -->
+              </div>
+
+              <? /* <div class='related-services'>
+                <h4>Related services:</h4>
+                <ol>
+                  <li><a href='#'>UIUX</a></li>
+                  <li><a href='#'>Cloud Infrastructure & Devops</a></li>
+                  <li><a href='#'>Maintenance of Legacy Code</a></li>
+                </ol>
+              </div> */ ?>
+
+              <div class='from-our-blog'>
+                <h4>From our engineering blog:</h4>
+                <ol>
+                  <li><a href='#'>Ruby templating: erb vs haml vs slim: an opinion</a></li>
+                  <li><a href='#'>How to split an app into gems</a></li>
+                  <li><a href='#'>Increasing prototyping speed: sql vs nosql</a></li>
+                </ol>
+              </div>
+
+            </div><!--col-1-->
+          </div><!--wco-card-4-->
+
+
+          <div class='wco-card-5 dev-wp'>
+
+            <div class='row-1'>
+              <div class='illustration'></div>
+              <div class='header'>
+                <h2>Wordpress</h2>
+                <div class='col-1'>
+                  <a href="/our-process-2"><button class="button-91" role="button">Bugfixes & Updates</button></a>
+                  <a href='/'>Create & Update Plugins</a>
+                </div>
+              </div>
+            </div><!--row-1-->
+            <div class='row row-2'>
+
+              <div class='related related-services col-md-6'>
+                <h4>Related Services:</h4>
+                <ol>
+                  <li><a href='#'>PHP Development</a></li>
+                  <li><a href='#'>CakePHP Development</a></li>
+                  <li><a href='#'>PHP testing w/composer</a></li>
+                  <li><a href='#'>Show More</a></li>
+                </ol>
+              </div>
+
+              <div class='related from-our-blog col-md-6'>
+                <h4>From our engineering blog:</h4>
+                <ol>
+                  <li><a href='#'>Setting up testing infrastructure for wordpress plugin development</a></li>
+                  <li><a href='#'>Deploying wordpress in the cloud with Docker</a></li>
+                  <li><a href='#'>Show More</a></li>
+                </ol>
+              </div>
+            </div><!--row-2-->
+
+          </div><!--wco-card-5-->
+
+
+          <div class='wco-card-4 dev-react'>
+
+            <div class='illustration'></div>
+
+            <div class='col-1'>
+
+              <div class='header'>
+                <h2>React Js</h2>
+                <a href="/our-process-2"><button class="button-91" role="button">Build a Frontend</button></a>
+                <!-- <a href='/'>View Offered Services</a> -->
+              </div>
+
+              <? /* <div class='related-services'>
+                <h4>Related services:</h4>
+                <ol>
+                  <li><a href='#'>node</a></li>
+                  <li><a href='#'>next_js</a></li>
+                  <li><a href='#'>es6 & babel</a></li>
+                  <li><a href='#'>socket & web rtcX</a></li>
+                  <li><a href='#'>Show More</a></li>
+                </ol>
+              </div> */ ?>
+
+              <div class='from-our-blog'>
+                <h4>From our engineering blog:</h4>
+                <ol>
+                  <li><a href='#'>Configure webpack build pipeline in production</a></li>
+                  <li><a href='#'>Resilience & future-proofing: typescript vs javascript</a></li>
+                  <li><a href='#'>Programming web sockets for RTC</a></li>
+                  <li><a href='#'>The NODE_ENV problem with CRA</a></li>
+                  <li><a href='#'>Show More</a></li>
+                </ol>
+              </div>
+
+            </div><!--col-1-->
+          </div><!--wco-card-3-->
+        </div>
+      </div><!--row-->
+    </section><!--dev-app-layer-md-->
+
+    <div class='spacer-500'></div>
+
+  </div><!--uniq id-->
+  <? get_footer(); ?>
+</body>
+
+</html>
