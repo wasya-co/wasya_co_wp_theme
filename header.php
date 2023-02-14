04cf3d4e1c26109b8a568705fdcb4047322bd651
mac_a2141 <victor@piousbox.com>
2023-02-14 11:51:10 -0500

    fix

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
 
   <link rel="stylesheet" href="/wp-content/themes/wasya_co_wp_theme/assets/css-compiled/a20230211-our-process-2.css">
   <link rel="stylesheet" href="/wp-content/themes/wasya_co_wp_theme/assets/css-compiled/a20230212-feb23-issue.css">
-  <link rel="stylesheet" href="/wp-content/themes/wasya_co_wp_theme/assets/css-compiled/a20230212-our-services2.css">
+  <link rel="stylesheet" href="/wp-content/themes/wasya_co_wp_theme/assets/css-compiled/a20230212-our-services-2.css">
   <link rel="stylesheet" href="/wp-content/themes/wasya_co_wp_theme/assets/css-compiled/a20230213-our-stack-2.css">
 
   <? wp_head(); ?>
