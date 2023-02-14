94c2b636779475499f2fd687b87127376f2a9894
mac_a2141 <victor@piousbox.com>
2023-02-13 19:50:57 -0500

    our-stack-2

---
+++ page-our-stack-2.php
@@ -0,0 +1,50 @@
+
+<html>
+  <head>
+    <link rel=stylesheet href="/wp-content/themes/wasya_co_wp_theme/assets/css-compiled/a20230213-our-stack-2.css">
+  </head>
+<body>
+
+<div class='spacer-300'></div>
+
+<div id='circularW' class='max-width' >
+  <div id='circular'>
+    <div class='el center'>
+      <div class='icon'></div>
+    </div>
+    <div class='el i1'>
+      <div class='icon'></div>
+      <div class='label'>
+        ES6 JavaScript, NPM Node packages
+      </div>
+    </div>
+    <div class='el i2'>
+      <div class='icon'></div>
+      <div class='label'>
+        React
+      </div>
+    </div>
+    <div class='el i3'>
+      <div class='icon'></div>
+      <div class='label'>
+        Webpack & Babel
+      </div>
+    </div>
+    <div class='el i4'>
+      <div class='icon'></div>
+      <div class='label'>
+        Jest & Testing
+      </div>
+    </div>
+    <div class='el i5'>
+      <div class='icon'></div>
+      <div class='label'>
+        Ionic
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class='spacer-300'></div>
+
+</body></html>
