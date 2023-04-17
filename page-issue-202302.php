0cd14a4f0e21755381899a72154cded0e27090f5
mac_a2141 <victor@piousbox.com>
2023-04-17 15:25:52 -0500

    minnor

--- page-issue-202302.php
+++ page-issue-202302.php
@@ -223,7 +223,8 @@
     <div class='max-width'>
       <div class='description'>
         <p class='sm' >Ready to make a move? <br />Chat with us about your project:</p>
-        <a href="/w/c2"><button class="button-91" role="button">Schedule a call</button></a>
+        <!-- <a href="/w/c2"><button class="button-91" role="button">Schedule a call</button></a> -->
+        <a href="/contact-us-4"><button class="button-91" role="button">Contact Us</button></a>
       </div>
       <div class='illustration'></div>
     </div>
