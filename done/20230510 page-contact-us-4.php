defb9eb323b08e8ba904a321464953c4df0a448e
mac_a2141 <victor@piousbox.com>
2023-05-10 16:09:02 -0500

    renaming things in done for archiving

---
+++ done/20230510 page-contact-us-4.php
@@ -0,0 +1,40 @@
+
+<? get_header(); ?>
+
+<article class='contact-us-3 max-width'>
+
+  <div style='height: 220px;' ></div>
+
+  <center><h1>Contact Us</h1></center>
+
+  <div style='height: 20px;' ></div>
+
+  <p>Due to an overwhelming volume of calls we've received, we have adjusted our preferred contact methods to include the following.</p>
+
+  <p>You can fill out the project intake form, which is our preferred method of contact:</p>
+
+  <a href="/w/c"><button class="button-91" role="button">Project Intake Form</button></a>
+
+  <div style='height: 60px;' ></div>
+
+  <p>You can always send us a message, and we shall respond promptly.</p>
+
+  <button class="button-91 popmake-3869" role="button">Send a Message</button>
+
+  <div style='height: 60px;' ></div>
+
+  <p>You can use our preferred method of contact and book a priority call:</p>
+
+  <a href="/w/c2"><button class="button-91" role="button">Book a priority call</button></a>
+
+  <div style='height: 60px;' ></div>
+
+  <p>Or, you can book a basic call. <br />Please note that contacting us via a basic call, does not guarantee a priority execution of your request.</p>
+
+  <a href="https://calendly.com/piousbox" target="_blank" style="padding: 1em; border: 1px solid black; border-radius: 15px" >Book a basic call</a>
+
+  <div style='height: 160px;' ></div>
+
+</article>
+
+<? get_footer(); ?>
