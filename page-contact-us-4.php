f35053c268e3a53190de936fe091d6734572330f
mac_a2141 <victor@piousbox.com>
2023-05-10 16:37:06 -0500

    contact-4-step1

--- page-contact-us-4.php
+++ page-contact-us-4.php
@@ -1,40 +1,18 @@
 
 <? get_header(); ?>
 
-<article class='contact-us-3 max-width'>
+<article class='contact-us-4 max-width'>
+  <div class='contact-us-4-C'>
 
-  <div style='height: 220px;' ></div>
+    <h1>Contact Us</h1>
 
-  <center><h1>Contact Us</h1></center>
+    <p>At Wasya Co, we understand the diverse needs of our clients, which is why we offer a range of contact methods. Whether it's through phone, email, live chat, or social media, we ensure that individuals can reach out in a way that aligns with their preferences and helps us provide them with the best possible support.</p>
 
-  <div style='height: 20px;' ></div>
+    <a href="/contact-us-4-step1">
+      <button class="button-91 popmake-3869" role="button">Continue</button>
+    </a>
 
-  <p>Due to an overwhelming volume of calls we've received, we have adjusted our preferred contact methods to include the following.</p>
-
-  <p>You can fill out the project intake form, which is our preferred method of contact:</p>
-
-  <a href="/w/c"><button class="button-91" role="button">Project Intake Form</button></a>
-
-  <div style='height: 60px;' ></div>
-
-  <p>You can always send us a message, and we shall respond promptly.</p>
-
-  <button class="button-91 popmake-3869" role="button">Send a Message</button>
-
-  <div style='height: 60px;' ></div>
-
-  <p>You can use our preferred method of contact and book a priority call:</p>
-
-  <a href="/w/c2"><button class="button-91" role="button">Book a priority call</button></a>
-
-  <div style='height: 60px;' ></div>
-
-  <p>Or, you can book a basic call. <br />Please note that contacting us via a basic call, does not guarantee a priority execution of your request.</p>
-
-  <a href="https://calendly.com/piousbox" target="_blank" style="padding: 1em; border: 1px solid black; border-radius: 15px" >Book a basic call</a>
-
-  <div style='height: 160px;' ></div>
-
+  </div>
 </article>
 
 <? get_footer(); ?>
