3e709ef4726c8ced8438d8f5b291a90ce155fd32
mac_a2141 <victor@piousbox.com>
2023-05-11 13:40:36 -0500

    moving pages

--- page-contact-us.php
+++ page-contact-us.php
@@ -8,7 +8,7 @@
 
     <p>At Wasya Co, we understand the diverse needs of our clients, which is why we offer a range of contact methods. Whether it's through phone, email, live chat, or social media, we ensure that individuals can reach out in a way that aligns with their preferences and helps us provide them with the best possible support.</p>
 
-    <a href="/contact-us-4-step1">
+    <a href="/contact-us/contact-us-step1">
       <button class="button-91 popmake-3869" role="button">Continue</button>
     </a>
 
