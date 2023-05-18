881402e4687f5a1fbb93c9ee5df1f19ade0fe3cd
mac_a2141 <victor@piousbox.com>
2023-05-18 13:36:54 -0500

    maybe a caching issue with redirects. groking our-services/

---
+++ page-our-services.php
@@ -0,0 +1,13 @@
+
+<? get_header(); ?>
+
+<? get_template_part( 'template-parts/our-services/content'); ?>
+<? get_template_part( 'template-parts/our-services/frontends'); ?>
+<? get_template_part( 'template-parts/our-services/dev-app-layer-sm'); ?>
+
+<? get_template_part( 'template-parts/our-services/dev-wp-h1'); ?>
+<? get_template_part( 'template-parts/our-services/dev-wp-md'); ?>
+<? get_template_part( 'template-parts/our-services/dev-react-md'); ?>
+<? get_template_part( 'template-parts/our-services/dev-ror-md'); ?>
+
+<? get_footer(); ?>
