a86cd748cab77a9b79c4aa390a7a5df3e3ae6376
mac_a2141 <victor@piousbox.com>
2023-07-19 16:04:59 -0500

    page culture-2. grapesjs is amazing

---
+++ page-culture-2.php
@@ -0,0 +1,239 @@
+<? get_header(); ?>
+
+<section id="igy3y" class="flex-sect">
+  <div class="container-width" id="ia969">
+    <div class="flex-title" id="i5hwk">Corporate Culture</div>
+    <div class="flex-desc" id="iz132">A successful company is greater than the sum of its parts.</div>
+    <div class="cards" id="imocj">
+      <div class="card" id="iq1kj">
+        <div class="card-header" id="i503h"></div>
+        <div class="card-body" id="iiix9">
+          <div class="card-title" id="i5tm9">Identity</div>
+          <div class="card-sub-title" id="ije6s">The ultimate driver of decisions</div>
+        </div><button type="button" id="i9ucby">Learn More</button>
+      </div>
+      <div class="card" id="i2lov">
+        <div class="card-header ch2" id="ijn7s"></div>
+        <div class="card-body" id="iv1cg">
+          <div class="card-title" id="i8sf4">Systems</div>
+          <div class="card-sub-title" id="ia1bd">The mechanics of interacting processes</div>
+        </div><button type="button" id="itz1ce">Learn More</button>
+      </div>
+      <div class="card" id="if9h7">
+        <div class="card-header ch3" id="iahzk"></div>
+        <div class="card-body">
+          <div class="card-title" id="i8ejl">Processes</div>
+          <div class="card-sub-title" id="i35ol">Mission-critical standardized routines</div>
+        </div><button type="button" id="ix62yk">Learn More</button>
+      </div>
+      <div class="card" id="iavsk">
+        <div class="card-header ch4" id="itt72"></div>
+        <div class="card-body">
+          <div class="card-title" id="i2uwj">KPI's</div>
+          <div class="card-sub-title" id="i05bs">Why we are not always results-oriented</div>
+        </div><button type="button" id="iauv8v">Learn More</button>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="flex-sect" id="izt7w3">
+  <div class="container-width">
+    <div class="flex-title">Flex is the new black</div>
+    <div class="flex-desc">With flexbox system you're able to build complex layouts easily and with free
+      responsivity</div>
+    <div class="cards" id="ie0k9y">
+      <div class="card">
+        <div class="card-header"></div>
+        <div class="card-body">
+          <div class="card-title" id="iliy37">Culture &amp; Identity</div>
+          <div class="card-sub-title">Subtitle one</div>
+          <div class="card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore</div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header ch2"></div>
+        <div class="card-body">
+          <div class="card-title" id="izwdhu">System</div>
+          <div class="card-sub-title">Subtitle two</div>
+          <div class="card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore</div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header ch3"></div>
+        <div class="card-body">
+          <div class="card-title" id="it2f04">Process</div>
+          <div class="card-sub-title">Subtitle three</div>
+          <div class="card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore</div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header ch4"></div>
+        <div class="card-body">
+          <div class="card-title" id="imgbp3">KPI's</div>
+          <div class="card-sub-title" id="iuuies">Why we are not always results-oriented.</div>
+          <div class="card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore</div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header ch5"></div>
+        <div class="card-body">
+          <div class="card-title">Title five</div>
+          <div class="card-sub-title">Subtitle five</div>
+          <div class="card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore</div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header ch6"></div>
+        <div class="card-body">
+          <div class="card-title">Title six</div>
+          <div class="card-sub-title">Subtitle six</div>
+          <div class="card-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<div id="ii37fz" class="gjs-row">
+  <div class="gjs-cell" id="i0d68"><img id="iwdagb" src="https://d15g8hc4183yn4.cloudfront.net/wp-content/uploads/2023/07/19142458/20230718-tree-swamp.jpg" />
+  </div>
+  <div class="gjs-cell" id="ik4hw">
+    <div id="i9q4l3">Promoting Sustainable Growth</div><button type="button" id="igjsuz">Learn More</button>
+  </div>
+</div>
+<section id="isrvk" class="am-sect">
+  <div class="container-width">
+    <div class="am-container" id="i1x26"><img src="./img/phone-app.png" class="img-phone" />
+      <div class="am-content">
+        <div class="am-pre">ASSET MANAGER</div>
+        <div class="am-title">Manage your images with Asset Manager</div>
+        <div class="am-desc">You can create image blocks with the command from the left panel and edit them with
+          double click</div>
+        <div class="am-post">Image uploading is not allowed in this demo</div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="blk-sect" id="ixl7s">
+  <div class="container-width">
+    <div class="blk-title">Blocks</div>
+    <div class="blk-desc">Each element in HTML page could be seen as a block. On the left panel you could find
+      different kind of blocks that you can create, move and style</div>
+    <div class="price-cards">
+      <div class="price-card-cont">
+        <div class="price-card">
+          <div class="pc-title">Starter</div>
+          <div class="pc-desc">Some random list</div>
+          <div class="pc-feature odd-feat">+ Starter feature 1</div>
+          <div class="pc-feature">+ Starter feature 2</div>
+          <div class="pc-feature odd-feat">+ Starter feature 3</div>
+          <div class="pc-feature">+ Starter feature 4</div>
+          <div class="pc-amount odd-feat">$ 9,90/mo</div>
+        </div>
+      </div>
+      <div class="price-card-cont">
+        <div class="price-card pc-regular">
+          <div class="pc-title">Regular</div>
+          <div class="pc-desc">Some random list</div>
+          <div class="pc-feature odd-feat">+ Regular feature 1</div>
+          <div class="pc-feature">+ Regular feature 2</div>
+          <div class="pc-feature odd-feat">+ Regular feature 3</div>
+          <div class="pc-feature">+ Regular feature 4</div>
+          <div class="pc-amount odd-feat">$ 19,90/mo</div>
+        </div>
+      </div>
+      <div class="price-card-cont">
+        <div class="price-card pc-enterprise">
+          <div class="pc-title">Enterprise</div>
+          <div class="pc-desc">Some random list</div>
+          <div class="pc-feature odd-feat">+ Enterprise feature 1</div>
+          <div class="pc-feature">+ Enterprise feature 2</div>
+          <div class="pc-feature odd-feat">+ Enterprise feature 3</div>
+          <div class="pc-feature">+ Enterprise feature 4</div>
+          <div class="pc-amount odd-feat">$ 29,90/mo</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="bdg-sect" id="iz1w62">
+  <div class="container-width">
+    <h1 class="bdg-title">The team</h1>
+    <div class="badges">
+      <div class="badge">
+        <div class="badge-header"></div><img src="img/team1.jpg" class="badge-avatar" />
+        <div class="badge-body">
+          <div class="badge-name">Adam Smith</div>
+          <div class="badge-role">CEO</div>
+          <div class="badge-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore ipsum dolor sit</div>
+        </div>
+        <div class="badge-foot"><span class="badge-link">f</span><span class="badge-link">t</span><span class="badge-link">ln</span></div>
+      </div>
+      <div class="badge">
+        <div class="badge-header"></div><img src="img/team2.jpg" class="badge-avatar" />
+        <div class="badge-body">
+          <div class="badge-name">John Black</div>
+          <div class="badge-role">Software Engineer</div>
+          <div class="badge-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore ipsum dolor sit</div>
+        </div>
+        <div class="badge-foot"><span class="badge-link">f</span><span class="badge-link">t</span><span class="badge-link">ln</span></div>
+      </div>
+      <div class="badge">
+        <div class="badge-header"></div><img src="img/team3.jpg" class="badge-avatar" />
+        <div class="badge-body">
+          <div class="badge-name">Jessica White</div>
+          <div class="badge-role">Web Designer</div>
+          <div class="badge-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore ipsum dolor sit</div>
+        </div>
+        <div class="badge-foot"><span class="badge-link">f</span><span class="badge-link">t</span><span class="badge-link">ln</span></div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="bdg-sect" id="i2a3nu">
+  <div class="container-width">
+    <h1 class="bdg-title">The team</h1>
+    <div class="badges">
+      <div class="badge">
+        <div class="badge-header"></div><img src="img/team1.jpg" class="badge-avatar" />
+        <div class="badge-body">
+          <div class="badge-name">Adam Smith</div>
+          <div class="badge-role">CEO</div>
+          <div class="badge-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore ipsum dolor sit</div>
+        </div>
+        <div class="badge-foot"><span class="badge-link">f</span><span class="badge-link">t</span><span class="badge-link">ln</span></div>
+      </div>
+      <div class="badge">
+        <div class="badge-header"></div><img src="img/team2.jpg" class="badge-avatar" />
+        <div class="badge-body">
+          <div class="badge-name">John Black</div>
+          <div class="badge-role">Software Engineer</div>
+          <div class="badge-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore ipsum dolor sit</div>
+        </div>
+        <div class="badge-foot"><span class="badge-link">f</span><span class="badge-link">t</span><span class="badge-link">ln</span></div>
+      </div>
+      <div class="badge">
+        <div class="badge-header"></div><img src="img/team3.jpg" class="badge-avatar" />
+        <div class="badge-body">
+          <div class="badge-name">Jessica White</div>
+          <div class="badge-role">Web Designer</div>
+          <div class="badge-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore ipsum dolor sit</div>
+        </div>
+        <div class="badge-foot"><span class="badge-link">f</span><span class="badge-link">t</span><span class="badge-link">ln</span></div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<? get_footer(); ?>
